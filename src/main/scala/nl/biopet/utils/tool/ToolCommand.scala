8d4eb22bb30eec6c638e92263f01d0a9ebe16d4f
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-11-16 13:55:47 +0100

    restructured definition order

--- src/main/scala/nl/biopet/utils/tool/ToolCommand.scala
+++ src/main/scala/nl/biopet/utils/tool/ToolCommand.scala
@@ -22,7 +22,7 @@ import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 /**
-  * Trait for biopet tools, sets some default args
+  * Trait for biopet tools, sets some default args, adds documentation generator
   */
 trait ToolCommand[Args] extends Logging {
 
@@ -34,6 +34,8 @@ trait ToolCommand[Args] extends Logging {
 
   /** This is the main entry point of the tool */
   def main(args: Array[String])
+
+  //ARGPARSER DEFINITION
 
   /** This is the parser object that will be tested. */
   def argsParser: AbstractOptParser[Args]
@@ -47,6 +49,83 @@ trait ToolCommand[Args] extends Logging {
       .parse(args, emptyArgs)
       .getOrElse(throw new IllegalArgumentException)
   }
+
+  // DOCUMENTATION TEXT
+
+  /** Force description to be written for each tool. */
+  def descriptionText: String
+
+  /** Force a manual to be written for each tool */
+  def manualText: String
+
+  /** Force an example to be written for each tool */
+  def exampleText: String
+
+  /** Universal text for pointing to the documentation.*/
+  def documentationText: String =
+    s"For documentation and manuals visit our [github.io page](https://biopet.github.io/$urlToolName)."
+
+  /** Universal contact text */
+  def contactText: String =
+    s"""
+       |<p>
+       |  <!-- Obscure e-mail address for spammers -->
+       |For any question related to this tool, please use the
+       |<a href='https://github.com/biopet/$urlToolName/issues'>github issue tracker</a>
+       |or contact
+       |  <a href='http://sasc.lumc.nl/'>the SASC team</a> directly at: <a href='&#109;&#97;&#105;&#108;&#116;&#111;&#58;
+       | &#115;&#97;&#115;&#99;&#64;&#108;&#117;&#109;&#99;&#46;&#110;&#108;'>
+       |  &#115;&#97;&#115;&#99;&#64;&#108;&#117;&#109;&#99;&#46;&#110;&#108;</a>.
+       |</p>
+       |
+     """.stripMargin
+
+  /** Universal text referring to BIOPET. */
+  def aboutText: String =
+    """
+      |This tool is part of BIOPET tool suite that is developed at LUMC by [the SASC team](http://sasc.lumc.nl/).
+      |Each tool in the BIOPET tool suite is meant to offer a standalone function that can be used to perform a
+      |dedicate data analysis task or added as part of [BIOPET pipelines](http://biopet-docs.readthedocs.io/en/latest/).
+    """.stripMargin
+
+  /** Universal installation text */
+  def installationText: String =
+    s"""
+       |This tool requires Java 8 to be installed on your device. Download Java 8
+       |[here](http://www.oracle.com/technetwork/java/javase/downloads/jre8-downloads-2133155.html)
+       |or install via your distribution's package manager.
+       |
+       |Download the latest version of $toolName [here](https://github.com/biopet/$urlToolName/releases/).
+       |To generate the usage run:
+       |
+       |    java -jar $toolName --version.jar --help
+       |
+    """.stripMargin
+
+  /** Which chapters should be in the README */
+  def readmeContents: List[(String, String)] = {
+    List(
+      (s"# $toolName", descriptionText),
+      ("# Documentation", documentationText),
+      ("# About", aboutText),
+      ("# Contact", contactText)
+    )
+  }
+
+  /** Which chapters should be on the tool documentation's main page */
+  def mainPageContents: List[(String, String)] = {
+    List(
+      (s"# $toolName", descriptionText),
+      ("# Installation", installationText),
+      ("# Manual", manualText),
+      ("## Example", exampleText),
+      ("## Usage", usageText),
+      ("# About", aboutText),
+      ("# Contact", contactText)
+    )
+  }
+
+  // DOCUMENTATION METHODS
 
   /** Creates a html formatted usage string */
   def usageText: String = {
@@ -124,66 +203,6 @@ trait ToolCommand[Args] extends Logging {
     table.toString.replace("\t", "  ")
   }
 
-  /** Force description to be written for each tool. */
-  def descriptionText: String
-
-  /** Force a manual to be written for each tool */
-  def manualText: String
-
-  /** Force an example to be written for each tool */
-  def exampleText: String
-
-  /** Universal text for pointing to the documentation.*/
-  def documentationText: String =
-    s"For documentation and manuals visit our [github.io page](https://biopet.github.io/$urlToolName)."
-
-  /** Universal contact text */
-  def contactText: String =
-    s"""
-       |<p>
-       |  <!-- Obscure e-mail address for spammers -->
-       |For any question related to this tool, please use the
-       |<a href='https://github.com/biopet/$urlToolName/issues'>github issue tracker</a>
-       |or contact
-       |  <a href='http://sasc.lumc.nl/'>the SASC team</a> directly at: <a href='&#109;&#97;&#105;&#108;&#116;&#111;&#58;
-       | &#115;&#97;&#115;&#99;&#64;&#108;&#117;&#109;&#99;&#46;&#110;&#108;'>
-       |  &#115;&#97;&#115;&#99;&#64;&#108;&#117;&#109;&#99;&#46;&#110;&#108;</a>.
-       |</p>
-       |
-     """.stripMargin
-
-  /** Universal text referring to BIOPET. */
-  def aboutText: String =
-    """
-      |This tool is part of BIOPET tool suite that is developed at LUMC by [the SASC team](http://sasc.lumc.nl/).
-      |Each tool in the BIOPET tool suite is meant to offer a standalone function that can be used to perform a
-      |dedicate data analysis task or added as part of [BIOPET pipelines](http://biopet-docs.readthedocs.io/en/latest/).
-    """.stripMargin
-
-  /** Universal installation text */
-  def installationText: String =
-    s"""
-       |This tool requires Java 8 to be installed on your device. Download Java 8
-       |[here](http://www.oracle.com/technetwork/java/javase/downloads/jre8-downloads-2133155.html)
-       |or install via your distribution's package manager.
-       |
-       |Download the latest version of $toolName [here](https://github.com/biopet/$urlToolName/releases/).
-       |To generate the usage run:
-       |
-       |    java -jar $toolName --version.jar --help
-       |
-    """.stripMargin
-
-  /** Which chapters should be in the README */
-  def readmeContents: List[(String, String)] = {
-    List(
-      (s"# $toolName", descriptionText),
-      ("# Documentation", documentationText),
-      ("# About", aboutText),
-      ("# Contact", contactText)
-    )
-  }
-
   /**
     * Generates a Markdown file from a list of chapters (heading, content) tuples.
     * @param contents A list of (string, string) tuples, where the first string is the title and the other the content.
@@ -232,17 +251,6 @@ trait ToolCommand[Args] extends Logging {
     */
   def generateDocumentation(outputDirectory: File): Unit = {
     outputDirectory.mkdirs()
-    val mainPageContents: List[(String, String)] = {
-      List(
-        (s"# $toolName", descriptionText),
-        ("# Installation", installationText),
-        ("# Manual", manualText),
-        ("## Example", exampleText),
-        ("## Usage", usageText),
-        ("# About", aboutText),
-        ("# Contact", contactText)
-      )
-    }
 
     contentsToMarkdown(mainPageContents,
                        new File(outputDirectory + "/index.md"))
@@ -262,9 +270,8 @@ trait ToolCommand[Args] extends Logging {
     val config: String = {
       s"""title = "$toolName"
          |
-          """.stripMargin + System.lineSeparator() +
-        "navigationOrder = [" + System.lineSeparator() +
-        navigationOrder + System.lineSeparator() + "]"
+          """.stripMargin + "\nnavigationOrder = [\n" +
+        navigationOrder + "\n]"
     }
     configFile.write(config)
     configFile.close()
