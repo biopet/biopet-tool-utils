391985af0983ed8eb64f0db5cbe9e084a274dc20
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-11-20 13:57:58 +0100

    change 'this tool' to

--- src/main/scala/nl/biopet/utils/tool/ToolCommand.scala
+++ src/main/scala/nl/biopet/utils/tool/ToolCommand.scala
@@ -69,7 +69,7 @@ trait ToolCommand[Args] extends Logging {
     s"""
        |<p>
        |  <!-- Obscure e-mail address for spammers -->
-       |For any question related to this tool, please use the
+       |For any question related to $toolName, please use the
        |<a href='https://github.com/biopet/$urlToolName/issues'>github issue tracker</a>
        |or contact
        |  <a href='http://sasc.lumc.nl/'>the SASC team</a> directly at: <a href='&#109;&#97;&#105;&#108;&#116;&#111;&#58;
@@ -81,8 +81,8 @@ trait ToolCommand[Args] extends Logging {
 
   /** Universal text referring to BIOPET. */
   def aboutText: String =
-    """
-      |This tool is part of BIOPET tool suite that is developed at LUMC by [the SASC team](http://sasc.lumc.nl/).
+    s"""
+      |$toolName is part of BIOPET tool suite that is developed at LUMC by [the SASC team](http://sasc.lumc.nl/).
       |Each tool in the BIOPET tool suite is meant to offer a standalone function that can be used to perform a
       |dedicate data analysis task or added as part of [BIOPET pipelines](http://biopet-docs.readthedocs.io/en/latest/).
     """.stripMargin
@@ -90,7 +90,7 @@ trait ToolCommand[Args] extends Logging {
   /** Universal installation text */
   def installationText: String =
     s"""
-       |This tool requires Java 8 to be installed on your device. Download Java 8
+       |$toolName requires Java 8 to be installed on your device. Download Java 8
        |[here](http://www.oracle.com/technetwork/java/javase/downloads/jre8-downloads-2133155.html)
        |or install via your distribution's package manager.
        |
